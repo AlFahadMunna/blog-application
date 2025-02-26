7bbab46b93913ef000e4d65c121fe78b2bb3735b
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-02-26 22:36:08 +0600

    some changes added

---
+++ components/dashboard/blog-dashboard.tsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { Clock, FileText, MessageCircle, PlusCircle } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import RecentArticles from "./RecentArticles";
+
+const BlogDashboard = () => {
+  return (
+    <main className="flex-1 p-4 md:p-8">
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <h1 className="font-bold text-2xl">Blog Dashboard</h1>
+          <p>Manage your content analytics</p>
+        </div>
+        <Link href="/dashboard/articles/create">
+          <Button>
+            <PlusCircle className="h-5 w-5" />
+            New Article
+          </Button>
+        </Link>
+      </div>
+
+      {/* quick stats*/}
+      <div className="grid gap-4 md:grid-cols-3 mb-8">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Total Articles
+            </CardTitle>
+            <FileText className="h-4 w-4" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">2</div>
+            <p className="text-xs text-muted-foreground mt-1">
+              +5 from last month
+            </p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Total Comments
+            </CardTitle>
+            <MessageCircle className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">20</div>
+            <p className="text-xs text-muted-foreground mt-1">
+              12 awaiting moderation
+            </p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Avg. Reading Time
+            </CardTitle>
+            <Clock className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">4.2m</div>
+            <p className="text-xs text-muted-foreground mt-1">
+              +0.8m from last month
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+
+      <RecentArticles />
+    </main>
+  );
+};
+
+export default BlogDashboard;

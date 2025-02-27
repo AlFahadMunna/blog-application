46bf67fb8078275c9db733c0ab07b50610579616
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-02-27 19:40:27 +0600

    create article page completed

--- components/articles/creata-articles-page.tsx
+++ components/articles/creata-articles-page.tsx
@@ -1,7 +1,60 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import dynamic from "next/dynamic";
+import { Button } from "../ui/button";
+import "react-quill-new/dist/quill.snow.css";
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 const CreateArticlesPage = () => {
-  return <div>CreateArticlesPage</div>;
+  const [content, setContent] = useState("");
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create New Article</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Input
+                type="text"
+                name="title"
+                placeholder="Enter a article title"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Category</Label>
+              <select className="flex h-10 w-full rounded-md border bg-background">
+                <option value="">Select category</option>
+                <option value="technology">Technology</option>
+                <option value="programming">Programming</option>
+                <option value="fashion">Fashion</option>
+              </select>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="featuredImage">Featured Image</Label>
+              <Input
+                type="file"
+                id="featuredImage"
+                name="featuredImage"
+                accept="image/*"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Content</Label>
+              <ReactQuill theme="snow" value={content} onChange={setContent} />
+            </div>
+            <div className="flex justify-end gap-4">
+              <Button variant={"outline"}>Cancel</Button>
+              <Button type="submit">Publish Article</Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default CreateArticlesPage;
